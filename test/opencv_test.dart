76659103f5156e30a9b486dc4a6f7d6c5a4c1a39
Arto Bendiken <arto@bendiken.net>
2018-11-15 18:50:48 +0200

    Fixed the test suite runner.

--- test/opencv_test.dart
+++ test/opencv_test.dart
@@ -1,9 +1,11 @@
 /* This is free and unencumbered software released into the public domain. */
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_opencv/opencv.dart';
 
 void main() {
-  // TODO
+  group("opencv", () {
+    // TODO
+  });
 }
