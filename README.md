1bcafb090d585c996427a6806d33487558f5fd09
Arto Bendiken <arto@bendiken.net>
2018-11-21 09:16:16 +0200

    Added a cross-reference table and links.

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ OpenCV Bindings for Flutter
 [![Dartdoc reference](https://img.shields.io/badge/dartdoc-reference-blue.svg)](https://pub.dartlang.org/documentation/flutter_opencv/latest/)
 [![Travis CI build status](https://img.shields.io/travis/drydart/flutter_opencv/master.svg)](https://travis-ci.org/drydart/flutter_opencv)
 
+Compatibility
+-------------
+
+Android only.
+
 Reference
 ---------
 
@@ -15,3 +20,11 @@ Reference
 
 - [`OpenCV.buildInformation`](https://pub.dartlang.org/documentation/flutter_opencv/latest/opencv/OpenCV/buildInformation.html)
 - [`OpenCV.versionString`](https://pub.dartlang.org/documentation/flutter_opencv/latest/opencv/OpenCV/versionString.html)
+
+Cross-Reference
+---------------
+
+| C++ | Flutter |
+| :--- | :--- |
+| `cv::getBuildInformation` | `OpenCV.buildInformation` |
+| `cv::getVersionString` | `OpenCV.versionString` |
--- lib/opencv.dart
+++ lib/opencv.dart
@@ -11,12 +11,18 @@ import 'package:flutter/services.dart' show MethodChannel;
 abstract class OpenCV {
   static const MethodChannel _channel = MethodChannel('flutter_opencv');
 
-  /// Returns in-depth OpenCV build information.
+  /// Returns full configuration-time `cmake` output.
+  ///
+  /// See: https://docs.opencv.org/3.4.3/db/de0/group__core__utils.html#ga0ae377100bc03ce22322926bba7fdbb5
   static Future<String> get buildInformation async {
     return await _channel.invokeMethod('getBuildInformation') as String;
   }
 
-  /// Returns the OpenCV version string.
+  /// Returns the library version string.
+  ///
+  /// For example, "3.4.3-dev".
+  ///
+  /// See: https://docs.opencv.org/3.4.3/db/de0/group__core__utils.html#gae87dff0eecfca4e5ec38ac06ee424980
   static Future<String> get versionString async {
     return await _channel.invokeMethod('getVersionString') as String;
   }
